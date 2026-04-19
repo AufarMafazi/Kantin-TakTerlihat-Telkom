30942940019cd1740dfa3fdee2307349f9806049
Ananda Alif Raditya <95111638+Allips28@users.noreply.github.com>
2026-04-19 19:20:09 +0700

    feat: inisialisasi project kantin online dari lokal ke repo Aufar

---
+++ Front-end/src/MainDashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Selamat Datang!</h1>
+    <p>Selamat datang di main dahboard.</p>
+  </div>
+</template>
+
+<script setup>
+// Anda bisa menambahkan logika JavaScript di sini nanti
+</script>
+
+<style scoped>
+/* Anda bisa menambahkan CSS khusus halaman ini di sini */
+h1 {
+  color: #42b983;
+}
+</style>
